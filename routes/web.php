b229c0c26fa4c46ac0296b2a9d6044a08425a717
Karl Murray <karlm89@me.com>
2021-02-25 12:24:02 -0600

    Get back to green.

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\ContestEntryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index.index');
 });
-
-Route::post('/contest', [ ContestEntryController::class, 'store' ]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::post('/contest', [ContestEntryController::class, 'store'])->name('store_contest');
 
 require __DIR__.'/auth.php';
